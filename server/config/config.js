8411f5bc018040d2de0610909ab094498b4cdb1b
Emmanuel <e.moralesi@alumnos.duoc.cl>
2020-11-06 16:57:49 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -5,6 +5,11 @@
 process.env.PORT = process.env.PORT || 3000;
 
 //===========================
+// ENTORNO
+//===========================
+process.env.NODE_ENV = process.env.NODE_ENV || dev
+
+//===========================
 // BASE DE DATOS
 //===========================
 
@@ -13,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://strider:9LDBiqfsUpVSF66t@cluster0.rpgf2.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI
 }
 
 process.env.URLDB = urlDB;
